5faef60d47bfb559535b1315ac79cbc31d41100c
TheLittleDevilYa <147209465+TheLittleDevilYa@users.noreply.github.com>
2026-03-24 23:06:06 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,11 @@
+-- Laucha Hub Loader
 
+print("Laucha Hub cargando...")
+
+local placeId = game.PlaceId
+
+if placeId == 0 then
+    print("Juego no detectado")
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TheLittleDevilYa/laucha-hub/main/universal.lua"))()
+end
